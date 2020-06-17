801527c4cc1830f0bc746825ca65d1e95ac53c60
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-17 10:49:01 -0500

    added a /

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import * as eventFiles from './events';
 // @ts-ignore
 import * as commandFiles from './commands';
 async function main(): Promise<void> {
-    const read = await fs.readFile(process.cwd() + 'config.json', 'utf8');
+    const read = await fs.readFile(process.cwd() + '/config.json', 'utf8');
     const config: { token: string, prefix: string, guildID: string, mongoDB: string, emailPass: string } = parse(read);
     const client: Client = new Client(config.token, { partials: ['MESSAGE','CHANNEL','REACTION'], ws: { intents: i} });
 
