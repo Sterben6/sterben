8d384b03ac6b7fb19da2fd557936f99958eb3035
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-17 15:49:06 -0500

    fixed jagc routing???

--- src/routes/jagc.ts
+++ src/routes/jagc.ts
@@ -3,7 +3,9 @@ import { Router } from 'express';
 const forceAuth = (req, res, next) => {
     if (!req.session.user) return res.redirect('/')
 }
-Router.get('/', (req, res) => {
+Router.get('/', function (req, res) {
     console.log('they went to jagc');
     res.send('enjoy bitch')
 });
+
+module.exports = Router;
