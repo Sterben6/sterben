e35861901ededa748ffb3e7f5a834c4e2dfc68c1
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-17 18:05:17 -0500

    ok fixed??

--- src/routes/jagc.ts
+++ src/routes/jagc.ts
@@ -7,7 +7,7 @@ const forceAuth = (req, res, next) => {
 }
 router.get('/', function (req, res) {
     console.log('they went to jagc');
-    res.render(process.cwd() + '/public/jagc/home', function (err, html) {
+    res.render(process.cwd() + '/public/jagc/home.html', function (err, html) {
         res.send(html)
     })
     // res.send('enjoy bitch')
