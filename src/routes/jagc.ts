edadad42129bf3f82c527d366e9cc5ccdc0d2fe0
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-17 18:23:13 -0500

    ok fixed??

--- src/routes/jagc.ts
+++ src/routes/jagc.ts
@@ -5,11 +5,10 @@ const router = new Router();
 const forceAuth = (req, res, next) => {
     if (!req.session.user) return res.redirect('/')
 }
-router.get('/', function (req, res, next) {
+router.get('/', function (next: () => void) {
     console.log('they went to jagc');
     // if you send here, next function won't be able to modify http headers
     // res.send('enjoy bitch')
-
     // next function tell express it needs to go to the next registered function for this path
     next()
 });
@@ -17,7 +16,7 @@ router.get('/', function (req, res, next) {
 router.use('/', express.static(process.cwd() + '/public/jagc'));
 
 router.get('/active', function (req, res) {
-
+    res.send('hi');
 });
 
 
