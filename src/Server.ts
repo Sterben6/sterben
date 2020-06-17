3d6c707569f1536223cd41ec2f1a92ab19dcdcc7
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-16 19:31:49 -0500

    Logging into port.

---
+++ src/Server.ts
@@ -0,0 +1,63 @@
+// Importing express related modules
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookeParser from 'cookie-parser';
+import helmet from 'helmet';
+// Import utilities
+import * as discord from 'discord.js';
+import fs from 'fs-extra';
+import * as moment from 'moment';
+import Signale from 'Signale';
+// Import storage & DB libraries
+import Redis from 'ioredis';
+import mongoose from 'mongoose';
+// Import internal items
+// @ts-ignore
+import config from '../config.json'
+export default class Server {
+    public config: { token: string; mongoURL: string;}
+    public app: express;
+    public client: discord.Client;
+    public urls: {api: string; web: string;};
+    public redis: Redis.Redis;
+    public signale: Signale.Signale
+    constructor() {
+        this.config = config;
+        this.redis = new Redis();
+        this.app = express()
+        this.signale = Signale;
+        this.signale.config({
+            displayFilename: true,
+            displayTimestamp: true,
+            displayDate: true,
+        });
+
+        this.client = new discord.Client();
+        this.connect()
+    }
+
+    private connect() {
+        mongoose.connect(this.config.mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        this.client.login(this.config.token).then(r => {
+            console.log(this.config.token == r)
+        });
+        this.app.set('trust proxy', 'loopback');
+        this.app.use(helmet({
+            hsts: false,
+            hidePoweredBy: false,
+            contentSecurityPolicy: {
+                directives: {
+                    defaultSrc: ["'self'"],
+                },
+            },
+            }));
+        this.app.use(bodyParser.json());
+        this.app.listen(8123, () => {
+            this.signale.success(`Server listening on port ${8123}`);
+        })
+
+    }
+}
