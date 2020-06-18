f11fcacdac03f04201c84c07d3dfdbeda5202dbc
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-17 22:10:34 -0500

    ????

--- src/routes/jagc.ts
+++ src/routes/jagc.ts
@@ -2,11 +2,18 @@ import { Router } from 'express';
 import express from 'express';
 import { Case } from '../models';
 const router = new Router();
+import path from 'path';
 const forceAuth = (req, res, next) => {
     if (!req.session.user) return res.redirect('/')
 }
-router.get('/', function (res: { sendFile: (arg0: string) => void; }, next: () => void) {
-    res.sendFile(process.cwd() + '/public/jagc/home.html')
+router.get('/', function (res: { render: (s: string) => void;
+    send(html: any): void;
+}, next: () => void) {
+    // @ts-ignore
+    res.render(path.join(process.cwd() + '/public/jagc/home.html', function (err, html): void {
+        if (err) return console.log(err);
+        res.send(html)
+    }))
 });
 
 
