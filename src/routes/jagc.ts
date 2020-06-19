c0941943fd5b2554610c367983f1e9e875e3ea15
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-19 16:25:20 -0500

    Removed unused parameters & imports

--- src/routes/jagc.ts
+++ src/routes/jagc.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import express from 'express';
 const router = new Router();
 import path from 'path';
-const forceAuth = (req, res, next) => {
+const forceAuth = (req, res) => {
     if (!req.session.user) return res.redirect('/')
 }
 const homePath = path.join(process.cwd() + '/public/jagc/home.html');
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.sendFile(homePath);
 })
 
@@ -17,7 +16,7 @@ router.get('/active', function (req, res) {
 
 router.get('/api/cases/:caseId', async (req, res) => {
     const caseid = req.params.caseId;
-    const caseObj = await this.client.db.Case.findOne({caseId: caseid});
+    const caseObj = await this.db.findOne({caseId: caseid});
     if (!caseObj) return res.status(404);
 })
 
