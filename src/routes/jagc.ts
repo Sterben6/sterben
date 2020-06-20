155bea96dd46852cb152b4a1c7852c14fbf6e47c
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-19 19:56:26 -0500

    Fixed schema?

--- src/routes/jagc.ts
+++ src/routes/jagc.ts
@@ -24,7 +24,7 @@ router.get('/active', function (req, res) {
 
 router.get('/api/cases/:caseId', async (req, res) => {
     await connect();
-    const modelSchema = Case.schema;
+    const modelSchema = Case;
 
     const caseid = req.params.caseId;
     const caseObj = modelSchema.findOne({caseId: caseid});
