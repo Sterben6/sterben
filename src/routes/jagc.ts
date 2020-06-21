3a143334ac2c3e5b9cea8e2d63007a86c424c58f
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-21 12:28:40 -0500

    Reusing jsx, and renamed home.html to index.html to begin using client side rendering.
    Signed-off-by: Sterben <47684081+Sterben6@users.noreply.github.com>

--- src/routes/jagc.ts
+++ src/routes/jagc.ts
@@ -12,7 +12,7 @@ async function connect() {
 const forceAuth = (req, res) => {
     if (!req.session.user) return res.redirect('/')
 }
-const homePath = path.join(process.cwd() + '/public/jagc/home.html');
+const homePath = path.join(process.cwd() + '/public/jagc/index.html');
 router.get('/', function (req, res) {
     res.sendFile(homePath);
 })
