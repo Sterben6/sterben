07318f54de91e3de8850af2adf3d7aaa5a8f822c
Sterben <47684081+Sterben6@users.noreply.github.com>
2020-06-26 22:15:35 -0500

    added rotech route shit
    Signed-off-by: Sterben <47684081+Sterben6@users.noreply.github.com>

--- src/routes/rotech.ts
+++ src/routes/rotech.ts
@@ -1,43 +1,9 @@
 import { Router } from 'express';
 const router = new Router();
-import path from 'path';
-import mongoose from 'mongoose';
-import config from '../../config.json';
-import { Case, ApiKey } from "../models";
 
-async function connect() {
-    await mongoose.connect(config.mongoURL, { useNewUrlParser: true, useUnifiedTopology: true});
-
-}
-const forceAuth = (req, res) => {
-    if (!req.session.user) return res.redirect('/')
-}
 router.get('/', function (req, res) {
     res.redirect('https://discord.gg/pDGCT3a')
 })
-
-/*
-router.get('/api/cases/:caseId?', async (req, res) => {
-    await connect();
-    const caseid = req.params.caseId;
-    const urlKey = req.query.apiKey;
-    if (!urlKey) return res.status(400).json({code: '401', message: 'UNAUTHORIZED'});
-    const keyObj = await ApiKey.findOne({ApiKey: urlKey});
-    const uses = keyObj.uses;
-    await keyObj.updateOne({ $set: {'uses': uses+1}});
-    if (!keyObj) return res.status(404).json({code: '401', message: 'UNAUTHORIZED'});
-    if (!caseid) return res.status(400).json({code: '400', message: 'BAD_REQUEST'});
-    const caseObj = await Case.findOne({caseId: caseid});
-    if (!caseObj) return res.status(404).json({code: '404', message: 'NOT_FOUND'});
-    console.log(caseObj)
-    await res.status(200).json({caseId: caseObj.caseId, creatorId: caseObj.creatorId, users: caseObj.users, charges: caseObj.charges})
-});
-// @ts-ignore
-import { Login } from "../../public/jagc/components/login"
-router.get('/login', function (req, res) {
-    res.render(Login)
-});
-*/
 
 
 
